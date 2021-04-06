0f49ceb15380fe400e12d1d4c50b84ffb84d4a80
sanshu-coder <82022392+sanshu-coder@users.noreply.github.com>
2021-04-06 13:58:24 +0530

    Initial Commit
    thi si smy commit

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Post, BlogComment
+
+admin.site.register((BlogComment))
+@admin.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js= ('tiny.js',)
